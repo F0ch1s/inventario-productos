416fa21c143c055c4e7c3e5beedbf807c14c0003
F0ch1s <rodrigoarenas975@gmail.com>
2026-06-12 15:37:14 -0500

    mas cambios

---
+++ dist/_astro/AppWrapper.ByY83-F4.js
@@ -0,0 +1,1 @@
+import{A as p}from"./AppWrapper.H6jhWnfT.js";import"./circle-alert.DN6sHF1x.js";import"./index.CyJuXTH7.js";export{p as default};
--- dist/_astro/AppWrapper.Ds2wYvZq.js
+++
@@ -1,1 +0,0 @@
-import{A as p}from"./AppWrapper.CXltQqsz.js";import"./circle-alert.DN6sHF1x.js";import"./index.CyJuXTH7.js";export{p as default};
